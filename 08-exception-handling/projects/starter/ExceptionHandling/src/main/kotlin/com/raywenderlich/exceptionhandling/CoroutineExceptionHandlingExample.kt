59e240ccdeafbee30f790455e3337fe68d187edd
Luka <luka.kordic@cobeisfresh.com>
2021-12-29 14:17:06 +0100

    Separate starter and final projects, update versions, add copyright

--- 08-exception-handling/projects/starter/ExceptionHandling/src/main/kotlin/com/raywenderlich/exceptionhandling/CoroutineExceptionHandlingExample.kt
+++ 08-exception-handling/projects/starter/ExceptionHandling/src/main/kotlin/com/raywenderlich/exceptionhandling/CoroutineExceptionHandlingExample.kt
@@ -1,33 +1,42 @@
+/*
+ * Copyright (c) 2022 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.raywenderlich.exceptionhandling
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.runBlocking
 
+@OptIn(DelicateCoroutinesApi::class)
 fun main() = runBlocking {
-  val asyncJob = GlobalScope.launch {
-    println("1. Exception created via launch coroutine")
 
-    // Will be printed to the console by
-    // Thread.defaultUncaughtExceptionHandler
-    throw IndexOutOfBoundsException()
-  }
-
-  asyncJob.join()
-  println("2. Joined failed job")
-
-  val deferred = GlobalScope.async {
-    println("3. Exception created via async coroutine")
-
-    // Nothing is printed, relying on user to call await
-    throw ArithmeticException()
-  }
-
-  try {
-    deferred.await()
-    println("4. Unreachable, this statement is never executed")
-  } catch (e: Exception) {
-    println("5. Caught ${e.javaClass.simpleName}")
-  }
 }
