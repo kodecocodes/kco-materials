f0123cdea1cd7d3fd897038a6866f62dcb76fdf5
Luka Kordić <lkordic@hotmail.com>
2022-01-10 22:37:46 +0100

    Add copyright, add final project, refactor examples

--- 09-manage-cancellation/projects/final/ManageCancellation/src/main/kotlin/com/raywenderlich/managecancellation/CancelAndJoinCoroutineExample.kt
+++ 09-manage-cancellation/projects/final/ManageCancellation/src/main/kotlin/com/raywenderlich/managecancellation/CancelAndJoinCoroutineExample.kt
@@ -33,8 +33,20 @@
  */
 package com.raywenderlich.managecancellation
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main() = runBlocking {
-
+  coroutineScope {
+    val job = launch {
+      repeat(1000) { i ->
+        println("$i. Crunching numbers [Beep.Boop.Beep]…")
+        delay(500L)
+      }
+    }
+    delay(1300L) // delay a bit
+    println("main: I am tired of waiting!")
+    // cancels the job and waits for job’s completion
+    job.cancelAndJoin()
+    println("main: Now I can quit.")
+  }
 }
--- 09-manage-cancellation/projects/final/ManageCancellation/src/main/kotlin/com/raywenderlich/managecancellation/CancelChildren.kt
+++ 09-manage-cancellation/projects/final/ManageCancellation/src/main/kotlin/com/raywenderlich/managecancellation/CancelChildren.kt
@@ -33,8 +33,44 @@
  */
 package com.raywenderlich.managecancellation
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import java.io.IOException
 
 fun main() = runBlocking {
+  val parentJob = launch {
+    val childOne = launch {
+      repeat(1000) { i ->
+        println("Child Coroutine 1: " +
+            "$i. Crunching numbers [Beep.Boop.Beep]…")
+        delay(500L)
+      }
+    }
 
+    // Handle the exception thrown from `launch`
+    // coroutine builder
+    childOne.invokeOnCompletion { exception ->
+      println("Child One: ${exception?.message}")
+    }
+
+    val childTwo = launch {
+      repeat(1000) { i ->
+        println("Child Coroutine 2: " +
+            "$i. Crunching numbers [Beep.Boop.Beep]…")
+        delay(500L)
+      }
+    }
+
+    // Handle the exception thrown from `launch`
+    // coroutine builder
+    childTwo.invokeOnCompletion { exception ->
+      println("Child Two: ${exception?.message}")
+    }
+
+  }
+  delay(1200L)
+
+  println("Calling cancelChildren() on the parentJob")
+  parentJob.cancelChildren()
+
+  println("parentJob isActive: ${parentJob.isActive}")
 }
