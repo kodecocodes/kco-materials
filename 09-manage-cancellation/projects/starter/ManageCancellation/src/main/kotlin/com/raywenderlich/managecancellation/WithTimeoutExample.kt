9469fce1d2247cca5d97aef5180c42cbad7f73ff
Luka Kordić <lkordic@hotmail.com>
2022-01-11 18:15:13 +0100

    Update starter project examples

--- 09-manage-cancellation/projects/starter/ManageCancellation/src/main/kotlin/com/raywenderlich/managecancellation/WithTimeoutExample.kt
+++ 09-manage-cancellation/projects/starter/ManageCancellation/src/main/kotlin/com/raywenderlich/managecancellation/WithTimeoutExample.kt
@@ -33,15 +33,8 @@
  */
 package com.raywenderlich.managecancellation
 
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
 
 fun main() = runBlocking {
-  withTimeout(1500L) {
-    repeat(1000) { i ->
-      println("$i. Crunching numbers [Beep.Boop.Beep]...")
-      delay(500L)
-    }
-  }
+
 }
