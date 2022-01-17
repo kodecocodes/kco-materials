2b493a8c057921511606f4ffc0e4c09c2a2b838b
Kevin Moore <kevindavmoore@gmail.com>
2022-01-17 11:15:30 -0800

    Add .gitignore, update main

--- 03-getting-started-with-coroutines/projects/starter/getting_started_with_coroutines/src/main/kotlin/Main.kt
+++ 03-getting-started-with-coroutines/projects/starter/getting_started_with_coroutines/src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ fun main() {
   (1..10000).forEach {
     GlobalScope.launch {
       val threadName = Thread.currentThread().name
-      println("$it printed on thread ${threadName}")
+      println("$it printed on thread $threadName")
     }
   }
   Thread.sleep(1000)
