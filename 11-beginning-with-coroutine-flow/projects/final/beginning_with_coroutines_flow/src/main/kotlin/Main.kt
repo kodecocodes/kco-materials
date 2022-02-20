54b8386e8acfb705c76d0c3c81a24bffbd35fc36
filbabic <fbabic96@gmail.com>
2022-02-20 17:49:37 +0100

    Update CH11

--- 11-beginning-with-coroutine-flow/projects/final/beginning_with_coroutines_flow/src/main/kotlin/Main.kt
+++ 11-beginning-with-coroutine-flow/projects/final/beginning_with_coroutines_flow/src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 fun main() {
@@ -13,36 +16,21 @@ fun main() {
   }
 
   GlobalScope.launch {
-    flowOfStrings.collect()
-  }
-
-//  GlobalScope.launch {
-//    flowOfStrings
-//        .map { it.split(" ") }
-//        .map { it.last() }
-//        .flowOn(Dispatchers.IO)
-//        .delayEach(100)
-//        .catch { }
-//        .flowOn(Dispatchers.Default)
-//        .collect { value ->
-//          println(value)
-//        }
-//  }
-
-  GlobalScope.launch {
     flowOfStrings
-        .map { it.split(" ") }
-        .map { it[1] }
-        .catch {
-          it.printStackTrace()
-          // send the fallback values
-          emit("Fallback")
-        }
-        .flowOn(Dispatchers.Default)
-        .collect { println(it) }
+      .map { it.split(" ") }
+      .map { it[1] }
+      .catch {
+        it.printStackTrace()
+        // send the fallback value or values
+        emit("Fallback")
+      }
+      .flowOn(Dispatchers.Default)
+      .collect { println(it) }
 
     println("The code still works!")
   }
 
-  Thread.sleep(2000)
+  Thread.sleep(1000)
 }
+
+
