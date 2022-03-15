14509d4427d6f6aca8d98fbc7bc4cc761235c198
Kevin Moore <kevindavmoore@gmail.com>
2022-03-14 21:22:57 -0700

    main should not have any code

--- 12-sharedflow-and-stateflow/projects/starter/sharedflow_and_stateflow/src/main/kotlin/Main.kt
+++ 12-sharedflow-and-stateflow/projects/starter/sharedflow_and_stateflow/src/main/kotlin/Main.kt
@@ -2,11 +2,4 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 fun main() {
-  (1..10000).forEach {
-    GlobalScope.launch {
-      val threadName = Thread.currentThread().name
-      println("$it printed on thread $threadName")
-    }
-  }
-  Thread.sleep(1000)
 }
